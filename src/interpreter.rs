49470d36b736c9f97b52de85652bdc81cbdbdff7
Hazurl <ricoujules69@gmail.com>
2018-08-02 19:15:53 +0200

    optimizer : compact & remove no-op

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,7 +3,6 @@ use super::instruction::*;
 use std::fmt;
 use std::io;
 use std::io::Read;
-use std::io::Write;
 
 pub struct Memory {
     memory: Vec<u8>,
@@ -19,7 +18,8 @@ impl Memory {
     }
 
     pub fn value(&mut self, offset: i32) -> &mut u8 {
-        &mut self.memory[Memory::check_ptr_overflow(self.ptr, offset)]
+        let p = Memory::check_ptr_overflow(self.ptr, offset, self.memory.len());
+        &mut self.memory[p]
     }
 
     pub fn add(&mut self, amount: i32, offset: i32) {
@@ -33,7 +33,7 @@ impl Memory {
     }
 
     pub fn move_ptr(&mut self, offset: i32) {
-        let ptr = Memory::check_ptr_overflow(self.ptr, offset);
+        let ptr = Memory::check_ptr_overflow(self.ptr, offset, self.memory.len());
         self.set_ptr(ptr);
     }
 
@@ -53,15 +53,15 @@ impl Memory {
         } as u8;
     }
 
-    pub fn check_ptr_overflow(ptr: usize, offset: i32) -> usize {
-        const MAX: i64 = usize::max_value() as i64;
+    pub fn check_ptr_overflow(ptr: usize, offset: i32, size: usize) -> usize {
+        let max: i64 = size as i64;
         const MIN: i64 = usize::min_value() as i64;
 
         let p = ptr as i64 + offset as i64;
-        return if p > MAX {
-            p % MAX
+        return if p > max {
+            p % max
         } else if p < MIN {
-            MAX - (-p % MAX)
+            max - (-p % max)
         } else {
             p
         } as usize;
@@ -99,8 +99,8 @@ impl State {
             Instruction::Read => self.memory.set(State::read(), 0),
             Instruction::Clear => self.memory.set(0, 0),
             Instruction::Copy(x) => {
-                let value = *self.memory.value(x);
-                self.memory.set(value, 0);
+                let value = *self.memory.value(0);
+                self.memory.set(value, x);
             }
             Instruction::JumpZero(x) => if *self.memory.value(0) == 0 {
                 self.ptr_code = x;
@@ -111,12 +111,15 @@ impl State {
         }
 
         self.ptr_code += 1;
-        println!("{:?}", self);
     }
 
     pub fn execute(&mut self, instrs: Vec<Instruction>) {
-        for i in instrs {
-            self.execute_once(i);
+        //println!("{:?}", self);
+        let range = 0..instrs.len();
+        while range.contains(&self.ptr_code) {
+            let p = self.ptr_code;
+            self.execute_once(instrs[p].clone());
+            //println!("{:?} after {:?}", self, instrs[p].clone());
         }
     }
 }
@@ -124,9 +127,6 @@ impl State {
 impl fmt::Debug for Memory {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let size = self.memory.len() as u64;
-        print!("{{{}, {}}} ~> ", size, self.ptr);
-        io::stdout().flush().expect("...");
-        println!("{}", ((self.ptr as u64 + size + 1) % size) as usize);
         write!(
             f,
             "[{}, {}, {}, {}, {}] around {}",
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+#![feature(range_contains)]
+#![feature(nll)]
+
 mod command;
 mod instruction;
 mod interpreter;
+mod optimizer;
 
 use command::*;
 use interpreter::*;
+use optimizer::*;
 
 fn main() {
     let mut state = State::new(1 << 16);
-    let is = parse("++++++++[->+<]>.").unwrap();
-    let cs = compile(is);
+    let is = parse("+- > +++++++++ [- >> +++++++++ <<] >>.").unwrap();
+    let better_is = remove_noop(compact(is));
+    let cs = compile(better_is);
     println!("{:?}", cs);
     state.execute(cs);
 }
